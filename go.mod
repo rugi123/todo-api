2040c77914f193076c4521cbfd9eb5395863cea2
tuskyp <bravl2010@bk.ru>
2025-07-07 16:51:40 +0300

    add config

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/rugi123/todo-api
 
 go 1.24.4
+
+require gopkg.in/yaml.v3 v3.0.1 // indirect
---
+++ internal/config/config.go
@@ -0,0 +1,50 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"gopkg.in/yaml.v3"
+)
+
+type PostgresConfig struct {
+	Host      string `yaml:"host"`
+	Port      int    `yaml:"port"`
+	User      string `yaml:"user"`
+	Password  string `yaml:"password"`
+	DBName    string `yaml:"dbname"`
+	TableName string `yaml:"tablename"`
+	SSLMode   string `yaml:"sslmode"`
+}
+
+type AppConfig struct {
+	Env       string        `yaml:"env"`
+	Port      string        `yaml:"port"`
+	JWTSecret string        `yaml:"jwt_secret"`
+	TokenTTL  time.Duration `yaml:"token_ttl"`
+}
+
+type Config struct {
+	App            AppConfig
+	PostgresConfig PostgresConfig
+}
+
+func (c *PostgresConfig) DSN() string {
+	return fmt.Sprintf(
+		"host=%s port=%d user=%s password=%s dbname=%s sslmode=%s",
+		c.Host, c.Port, c.User, c.Password, c.DBName, c.SSLMode,
+	)
+}
+
+func Load(path string) (*Config, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read config: %s", err)
+	}
+	cfg := Config{}
+	if err := yaml.Unmarshal(data, &cfg); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal config: %s", err)
+	}
+	return &cfg, nil
+}
